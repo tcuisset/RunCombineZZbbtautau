84aea5bc4917bbd74765eb33d3cb6f0aebef57d7
Elena Vernazza <e.vernazza1@campus.unimib.it>
2024-05-15 21:59:16 +0200

    Color blind plots with python

--- RunAsymptoticLimits.py
+++ RunAsymptoticLimits.py
@@ -1,8 +1,10 @@
 import os,json,concurrent.futures,itertools
 import pdb
 import numpy as np
-import ROOT
-ROOT.gROOT.SetBatch(True)
+
+import matplotlib.pyplot as plt
+import mplhep
+plt.style.use(mplhep.style.CMS)
 
 '''
 python3 RunAsymptoticLimits.py --cfg ul_2016_HIPM_ZZ_v12,ul_2016_ZZ_v12,ul_2017_ZZ_v12,ul_2018_ZZ_v12 \
@@ -209,11 +211,11 @@ def PlotAsymptoticLimits(base_dir, mass_points, prd, cfg, feat, prefix):
         folder_name = f'{cfg}/{prd}/combination'
         out_folder = f'{cfg}/{prd}'
 
-    if '2018' in cfg:          text_year = '2018 - 59.7 fb^{-1} (13 TeV)'
-    elif '2017' in cfg:        text_year = '2017 - 41.5 fb^{-1} (13 TeV)'
-    elif '2016' in cfg:        text_year = '2016Post - 16.8 fb^{-1} (13 TeV)'
-    elif '2016_HIPM' in cfg:   text_year = '2016Pre - 19.5 fb^{-1} (13 TeV)'
-    elif 'FullRun2' in cfg: text_year = 'Full Run 2 - 137.1 fb^{-1} (13 TeV)'
+    if '2018' in cfg:          text_year = r'2018 - 59.7 fb$^{-1}$ (13 TeV)'
+    elif '2017' in cfg:        text_year = r'2017 - 41.5 fb$^{-1}$ (13 TeV)'
+    elif '2016' in cfg:        text_year = r'2016 PostVFP - 16.8 fb$^{-1}$ (13 TeV)'
+    elif '2016_HIPM' in cfg:   text_year = r'2016 PreVFP - 19.5 fb$^{-1}$ (13 TeV)'
+    elif 'FullRun2' in cfg: text_year = r'Full Run 2 - 137.1 fb$^{-1}$ (13 TeV)'
     
     for mass_value in mass_points:
         limit_file = base_dir + f'/{folder_name}/{feat}_{mass_value}/limits.json'
@@ -221,139 +223,47 @@ def PlotAsymptoticLimits(base_dir, mass_points, prd, cfg, feat, prefix):
             mass_dict = json.load(json_file)
         m = list(mass_dict.keys())[0]
         first_key = list(mass_dict.keys())[0]
-        mass.append(m)
+        mass.append(float(m))
         exp.append(mass_dict[first_key]['exp'])
         m1s_t.append(mass_dict[first_key]['m1s_t'])
         p1s_t.append(mass_dict[first_key]['p1s_t'])
         m2s_t.append(mass_dict[first_key]['m2s_t'])
         p2s_t.append(mass_dict[first_key]['p2s_t'])
 
-    mass  = np.array(mass, dtype=float)
-    exp   = np.array(exp, dtype=float)
-    m1s_t = np.array(m1s_t, dtype=float)
-    p1s_t = np.array(p1s_t, dtype=float)
-    m2s_t = np.array(m2s_t, dtype=float)
-    p2s_t = np.array(p2s_t, dtype=float)  
-        
-    G_exp = ROOT.TGraph()
-    G_sig1 = ROOT.TGraphAsymmErrors()
-    G_sig2 = ROOT.TGraphAsymmErrors()
+    # Setup matplotlib figure
+    fig, ax = plt.subplots(figsize=(12,10))
 
-    ipt = 0
-    for m, e, m1_t, p1_t, m2_t, p2_t in zip(mass, exp, m1s_t, p1s_t, m2s_t, p2s_t):
+    ax.plot(mass, exp, color='k', marker='o', label = "Expected", zorder=10)
+    ax.fill_between(np.asarray(mass), 
+                    np.asarray(p1s_t), 
+                    np.asarray(m1s_t), color = '#FFDF7Fff', label = "68% expected", zorder=3)
+    ax.fill_between(np.asarray(mass), 
+                    np.asarray(p2s_t), 
+                    np.asarray(m2s_t), color = '#85D1FBff', label = "95% expected")
 
-        p2 = p2_t - e
-        p1 = p1_t - e
-        m2 = e - m2_t
-        m1 = e - m1_t
+    mplhep.cms.label(data=False, rlabel=text_year, fontsize=22)
 
-        G_exp.SetPoint(ipt, m, e)
-        G_sig1.SetPoint(ipt, m, e)
-        G_sig1.SetPointError(ipt, 0, 0, m1, p1)
-        G_sig2.SetPoint(ipt, m, e)
-        G_sig2.SetPointError(ipt, 0, 0, m2, p2)
-
-        ipt += 1
-
-    G_exp.SetMarkerStyle(24)
-    G_exp.SetMarkerColor(4)
-    G_exp.SetMarkerSize(0.8)
-    G_exp.SetLineColor(ROOT.kBlack)
-    G_exp.SetLineWidth(3)
-    G_exp.SetLineStyle(2)
-    G_exp.SetFillColor(0)
-
-    G_sig1.SetMarkerStyle(0)
-    G_sig1.SetMarkerColor(3)
-    G_sig1.SetFillColor(ROOT.kGreen+1)
-    G_sig1.SetLineColor(ROOT.kGreen+1)
-    G_sig1.SetFillStyle(1001)
-    
-    G_sig2.SetMarkerStyle(0)
-    G_sig2.SetMarkerColor(5)
-    G_sig2.SetFillColor(ROOT.kOrange)
-    G_sig2.SetLineColor(ROOT.kOrange)
-    G_sig2.SetFillStyle(1001)
-
-    canvas = ROOT.TCanvas('canvas', 'canvas', 650, 500)
-    canvas.SetFrameLineWidth(3)
-    canvas.SetBottomMargin(0.15)
-    canvas.SetRightMargin(0.05)
-    canvas.SetLeftMargin(0.15)
-    canvas.SetGridx()
-    canvas.SetGridy()
-
-    # Outside frame
-    x_min = np.min(mass) - 30
-    x_max = np.max(mass) + 30
-    frame_bounds = x_min, x_max
-    hframe = ROOT.TH1F('hframe', '',
-                    100, frame_bounds[0], frame_bounds[1])
-    hframe.SetMinimum(0.1)
-    hframe.GetYaxis().SetRangeUser(0.005,5)
-
-    hframe.GetYaxis().SetTitleSize(0.047)
-    hframe.GetXaxis().SetTitleSize(0.055)
-    hframe.GetYaxis().SetLabelSize(0.045)
-    hframe.GetXaxis().SetLabelSize(0.045)
-    hframe.GetXaxis().SetLabelOffset(0.012)
-    hframe.GetYaxis().SetTitleOffset(1.2)
-    hframe.GetXaxis().SetTitleOffset(1.1)
     if "ZZ" in prefix:
-        process_tex = "X#rightarrowZZ#rightarrow bb#tau#tau"
-        hframe.GetXaxis().SetTitle("m_{X} [GeV]")
+        process_tex = r"$X\rightarrow ZZ\rightarrow bb\tau\tau$"
+        ax.set_xlabel(r"$m_{X}$ [GeV]")
     elif "ZbbHtt" in prefix:
-        process_tex = "Z'#rightarrowZH#rightarrow bb#tau#tau"
-        hframe.GetXaxis().SetTitle("m_{Z'} [GeV]")
+        process_tex = r"$Z'\rightarrow ZH\rightarrow bb\tau\tau$"
+        ax.set_xlabel(r"$m_{Z'}$ [GeV]")
     elif "ZttHbb" in prefix:
-        process_tex = "Z'#rightarrowZH#rightarrow #tau#tau bb"
-        hframe.GetXaxis().SetTitle("m_{Z'} [GeV]")
+        process_tex = r"$Z'\rightarrow ZH\rightarrow \tau\tau bb$"
+        ax.set_xlabel(r"$m_{Z'}$ [GeV]")
     else:
         process_tex = "unknown process"
-    hframe.GetYaxis().SetTitle("95% CL on #sigma #times #bf{#it{#Beta}}(" + process_tex + ") [pb]")
+    ax.set_ylabel(r"95% CL on $\sigma \times \mathbf{\it{B}}$(" + process_tex + r") [pb]")
     
-    hframe.SetStats(0)
-    ROOT.gPad.SetTicky()
-    hframe.Draw()
-    hframe.GetYaxis().SetRangeUser(0.005,5)
+    # Style
+    ax.legend()
+    ax.set_yscale('log')
 
-    ptext1 = ROOT.TPaveText(0.1663218-0.02, 0.886316, 0.3045977-0.02, 0.978947, 'brNDC')
-    ptext1.SetBorderSize(0)
-    ptext1.SetTextAlign(12)
-    ptext1.SetTextFont(62)
-    ptext1.SetTextSize(0.05)
-    ptext1.SetFillColor(0)
-    ptext1.SetFillStyle(0)
-    ptext1.AddText('CMS #font[52]{Internal}')
-    ptext1.Draw()
+    fig.savefig(base_dir + f'/{out_folder}/Limits_'+feat+'_'+prd+'_'+cfg+'.png')
+    print(base_dir + f'/{out_folder}/Limits_'+feat+'_'+prd+'_'+cfg+'.png')
 
-    ptext2 = ROOT.TPaveText(0.7, 0.91, 0.85, 0.95, 'brNDC')
-    ptext2.SetBorderSize(0)
-    ptext2.SetFillColor(0)
-    ptext2.SetTextSize(0.040)
-    ptext2.SetTextFont(42)
-    ptext2.SetFillStyle(0)
-    ptext2.AddText(text_year)
-    ptext2.Draw()
 
-    legend = ROOT.TLegend(0.7,0.7,0.9,0.89)
-    legend.SetBorderSize(0)
-    legend.AddEntry(G_sig1, '68% exp.', 'f')
-    legend.AddEntry(G_sig2, '95% exp.', 'f')
-    legend.AddEntry(G_exp, 'Expected', 'l')
-    G_sig2.Draw('3same')
-    G_sig1.Draw('3same')
-    G_exp.Draw('Lsame')
-    
-    canvas.Update()
-    canvas.SetLogy()
-
-    legend.Draw()
-
-    save_name = base_dir + f'/{out_folder}/Limits_'+feat+'_'+prd+'_'+cfg
-    canvas.SaveAs(save_name+'.png')
-    canvas.SaveAs(save_name+'.pdf')
-    
 if __name__ == "__main__" :
 
     from optparse import OptionParser
